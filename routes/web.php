ce8dd095fb6ed2ecc9a3e7f9e59fb3685cf6e87a
Tanjin Adnan Abir <tanjinadnanabir@gmail.com>
2022-04-22 00:00:50 +0600

    post blade updated

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', [PostController::class, 'index'])->name('post_index');
+Route::post('/post', [PostController::class, 'create'])->name('post_create');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
